d7236aa1cf7c7620ed56a25247182e6ff942106b
shiyi <56289964@qq.com>
2019-07-26 17:58:09 +0800

    ADD JavaScript

---
+++ base/base-HTML/src/main/java/html/base/javascript/JavaScript.java
@@ -0,0 +1,15 @@
+package html.base.javascript;
+
+import j2html.tags.ContainerTag;
+
+/**
+ * @author yshi
+ *
+ */
+public class JavaScript extends ContainerTag{
+
+	public JavaScript() {
+		super("script");
+	}
+
+}

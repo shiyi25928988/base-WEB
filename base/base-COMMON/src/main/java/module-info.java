0e22619568b55aa75b34e47deb0302e7b3ce1a1f
shiyi25928988 <shiyi25928988@163.com>
2019-11-07 10:23:24 +0800

    1234

---
+++ base/base-COMMON/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module base.common.module {
+	exports base.http;
+	requires org.apache.commons.lang3;
+	requires org.apache.httpcomponents.httpcore;
+	requires org.apache.httpcomponents.httpclient;
+}
--- base/base-NEUROPH/src/main/java/neuroph/base/TestAnd.java
+++ base/base-NEUROPH/src/main/java/neuroph/base/TestAnd.java
@@ -1,5 +1,16 @@
 package neuroph.base;
 
+import org.neuroph.core.NeuralNetwork;
+import org.neuroph.core.data.DataSet;
+import org.neuroph.core.data.DataSetRow;
+import org.neuroph.nnet.MultiLayerPerceptron;
+import org.neuroph.util.TransferFunctionType;
+
 public class TestAnd {
-	
+	public static void main(String...strings) {
+		DataSet trainingDataSet = new DataSet(2, 1);
+		trainingDataSet.add(new DataSetRow("", ""));
+		
+		NeuralNetwork myPerceptron = new MultiLayerPerceptron(TransferFunctionType.SIGMOID);
+	}
 }

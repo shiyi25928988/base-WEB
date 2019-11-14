aeeb90953d5f0b08331e52410c60c2d524bec012
shiyi25928988 <shiyi25928988@163.com>
2019-11-14 17:18:21 +0800

    1312

--- base/base-CRAWLER/src/main/java/base/crawler/config/CrawlerConstants.java
+++ base/base-CRAWLER/src/main/java/base/crawler/config/CrawlerConstants.java
@@ -1,10 +1,12 @@
 package base.crawler.config;
 
+import java.io.File;
+
 public interface CrawlerConstants {
 	
 	public static final int NUMBER_OF_CRAWLERS = 1;
 	
-	public static final String CURRENT_PATH = System.getProperty("user.dir"); 
+	public static final String CURRENT_PATH = System.getProperty("user.dir") + File.separator + "crawl" ; 
 	
 	public static final int MESSAGE_QUEUE_SIZE = 100;
 }

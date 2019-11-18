ff593cebb91724fa731f4d2d8fdf21fb96de2cc5
shiyi25928988 <shiyi25928988@163.com>
2019-11-18 17:58:34 +0800

    312312

--- base/base-CRAWLER/src/main/java/base/crawler/config/ContentType.java
+++ base/base-CRAWLER/src/main/java/base/crawler/config/ContentType.java
@@ -15,6 +15,43 @@ public abstract class ContentType {
 		typeMap.put("audio/aac", ".aac");
 		
 		typeMap.put("application/x-abiword", ".abw");
+		
+		typeMap.put("application/x-freearc", ".arc");
+		
+		typeMap.put("video/x-msvideo", ".avi");
+		
+		typeMap.put("application/vnd.amazon.ebook", ".azw");
+		  
+		typeMap.put("application/octet-stream", ".bin");
+		
+		typeMap.put("image/bmp", ".bmp");
+		
+		typeMap.put("application/x-bzip", ".bz");
+		
+		typeMap.put("application/x-bzip2", ".bz2");
+		
+		typeMap.put("application/x-csh", ".csh");
+		
+		typeMap.put("text/css", ".css"); 
+		
+		typeMap.put("text/csv", ".csv"); 
+		
+		typeMap.put("application/msword", ".doc"); 
+		
+		typeMap.put("application/vnd.openxmlformats-officedocument.wordprocessingml.document", ".docx"); 
+		
+		typeMap.put("application/vnd.ms-fontobject", ".eot");
+		
+		typeMap.put("application/epub+zip", ".epub");
+		
+		typeMap.put("image/gif", ".gif");
+		
+		typeMap.put("text/html", ".html");
+		
+		typeMap.put("image/vnd.microsoft.icon", ".ico");
+		
+		typeMap.put("text/calendar", ".ics");
+		
 	}
 
 	

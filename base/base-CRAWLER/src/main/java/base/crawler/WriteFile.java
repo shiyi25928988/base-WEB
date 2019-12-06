c02128eb04d5f5c60a2e37032ea927b2f9a25366
shiyi25928988 <shiyi25928988@163.com>
2019-12-06 15:13:29 +0800

    crawler/WriteFile.java

--- base/base-CRAWLER/src/main/java/base/crawler/WriteFile.java
+++ base/base-CRAWLER/src/main/java/base/crawler/WriteFile.java
@@ -5,11 +5,13 @@ import java.util.regex.Pattern;
 
 import base.crawler.config.CrawlerConstants;
 import base.io.IOUtils;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author yshi
  *
  */
+@Slf4j
 public abstract class WriteFile {
 
 	private static Pattern filePattern = Pattern.compile("[\\\\/:*?\"<>|]");
@@ -18,15 +20,25 @@ public abstract class WriteFile {
 	 * @param result
 	 */
 	public static void writeResult(CrawlResults result) {
-		
+
+		switch (result.getType()) {
+		case Html:
+		case Text:
+			break;
+		case Binary:
+			break;
+			default:
+		}
+
 		var extension = result.getExtension();
-		
-		var realPath = CrawlerConstants.CURRENT_PATH + File.separator + result.getRootFolder() + result.getFileName().substring(0, result.getFileName().lastIndexOf("/")).replace("/", File.separator);
+
+		var realPath = CrawlerConstants.CURRENT_PATH + File.separator + result.getRootFolder()
+				+ result.getFileName().substring(0, result.getFileName().lastIndexOf("/")).replace("/", File.separator);
 
 		var fileName = result.getFileName().substring(result.getFileName().lastIndexOf("/") + 1);
-		
+
 		fileName = filePattern.matcher(fileName).replaceAll("_");
-		
+
 		var content = result.getContent();
 
 		IOUtils.writeBinaryFile(content, realPath, fileName + extension);

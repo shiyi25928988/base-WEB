d9c95d5a71d56313b05da3f03753cbede51c875d
shiyi25928988 <shiyi25928988@163.com>
2019-12-17 17:56:32 +0800

    1234

--- base/base-NEUROPH/src/main/java/neuroph/base/AndPerceptronNoLearn.java
+++ base/base-NEUROPH/src/main/java/neuroph/base/AndPerceptronNoLearn.java
@@ -1,11 +1,19 @@
 package neuroph.base;
 
+import org.neuroph.core.Layer;
 import org.neuroph.core.NeuralNetwork;
+import org.neuroph.nnet.comp.neuron.BiasNeuron;
+import org.neuroph.util.LayerFactory;
 import org.neuroph.util.NeuralNetworkType;
+import org.neuroph.util.NeuronProperties;
 
+/**
+ * @author yshi
+ *
+ */
 @SuppressWarnings("rawtypes")
-public class AndPerceptronNoLearn extends NeuralNetwork{
-	
+public class AndPerceptronNoLearn extends NeuralNetwork {
+
 	/**
 	 * 
 	 */
@@ -13,6 +21,16 @@ public class AndPerceptronNoLearn extends NeuralNetwork{
 
 	private void createNetwork(int inputNeuronsCount) {
 		this.setNetworkType(NeuralNetworkType.PERCEPTRON);
+
+		NeuronProperties inputNeuronProperties = new NeuronProperties();
+		// inputNeuronProperties.setProperty(key, value);
+		NeuronProperties outputNeuronProperties = new NeuronProperties();
+
+		Layer inputLayer = LayerFactory.createLayer(inputNeuronsCount, inputNeuronProperties);
+
+		this.addLayer(inputLayer);
+
+		inputLayer.addNeuron(new BiasNeuron());
 	}
 
 }

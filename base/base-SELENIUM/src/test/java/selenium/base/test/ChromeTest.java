37ebefdebce095377f43c284d282bb274f18ce57
shiyi25928988 <shiyi25928988@163.com>
2019-12-19 17:44:36 +0800

    1234

--- base/base-SELENIUM/src/test/java/selenium/base/test/ChromeTest.java
+++ base/base-SELENIUM/src/test/java/selenium/base/test/ChromeTest.java
@@ -4,37 +4,42 @@ import java.io.File;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ChromeTest {
-	
-	private static final String driverPath = System.getProperty("user.dir") 
-			+ File.separator + "src" 
-			+ File.separator + "main" 
-			+ File.separator + "resources" 
-			+ File.separator + "drivers"
-			+ File.separator + "chrome"
+
+	private static final String driverPath = System.getProperty("user.dir") + File.separator + "src" + File.separator
+			+ "main" + File.separator + "resources" + File.separator + "drivers" + File.separator + "chrome"
 			+ File.separator + "chromedriver.exe";
 	static {
 		System.setProperty("webdriver.chrome.driver", driverPath);
 	}
 	private static WebDriver driver = new ChromeDriver();
-	
-	public static void main(String...strings) {
-		//Convenient
-		driver.get("https://selenium.dev");
 
-		//Longer way
-		driver.navigate().to("https://selenium.dev");
+	public static void main(String... strings) {
+//		// Convenient
+//		driver.get("https://selenium.dev");
+//
+//		// Longer way
+//		driver.navigate().to("https://selenium.dev");
+//
+//		driver.getCurrentUrl();
+//
+//		driver.navigate().back();
+//
+//		driver.navigate().forward();
+//
+//		driver.navigate().refresh();
+//
+//		driver.getTitle();
+
+		driver.get("http://www.google.com");
+		WebElement searchForm = driver.findElement(By.tagName("form"));
+		WebElement searchBox = searchForm.findElement(By.name("q"));
+		searchBox.sendKeys("webdriver");
 		
-		driver.getCurrentUrl();
-		
-		driver.navigate().back();
-		
-		driver.navigate().forward();
-		
-		driver.navigate().refresh();
-		
-		driver.getTitle();
+		WebElement button  = searchForm.findElement(By.name("btnK"));
+		button.click();
 	}
 }

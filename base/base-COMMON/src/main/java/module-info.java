43f0e960828ef57787e1748073e03274c2410ba6
shiyi <shiyi25928988@163.com>
2020-03-14 10:02:06 +0800

    111

--- base/base-COMMON/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module base.common {
-	requires org.apache.commons.lang3;
-	requires org.apache.httpcomponents.httpcore;
-	requires org.apache.httpcomponents.httpclient;
-	requires java.desktop;
-	requires lombok;
-	requires com.fasterxml.jackson.core;
-}

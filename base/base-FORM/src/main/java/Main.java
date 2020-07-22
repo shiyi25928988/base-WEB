a3fca0e2515af3c7005d78407197df6456fd7de8
shiyi <shiyi25928988@163.com>
2020-07-22 18:15:09 +0800

    123

---
+++ base/base-FORM/src/main/java/Main.java
@@ -0,0 +1,51 @@
+
+public class Main {
+
+	public static void main(String... strings) {
+		String inputStr = "(frrCode=R-15DC0003089, frrFileName=8080e986-0ce0-46db-ba58-cb2bfae85e7e.pdf, frrFileUrl=http://comac-mom-prod.oss-cn-shanghai-internal.aliyuncs.com/FRR_PDF/PDF_FILES/8080e986-0ce0-46db-ba58-cb2bfae85e7e.pdf, drawCode=, drawFileName=null, drawFileUrl=null, sortie=, dataFrom=null, sendTime=2020-07-09 16:10:00, state=, frrId=352, frrType=FRR非转移, frrFrom=null, frrLoginuser=201514, aircraftType=C919, disposition=DispositionDTO(useAsIt=2, rework=, repair=, scrap=, secondTestPassed=, returnToSupplier=, initialTreatment=), respDepartmentName=SUPP, keyItem=一般件, discrepancy=严重不合格, enginePicExist=null, enginePinguExist=null, engineAttachExist=null, engineFileExist=null, correctFileExist=null, scrapFileExist=null, frrPicExist=http://comac-mom-prod.oss-cn-shanghai-internal.aliyuncs.com/MAIN_FRR_RECORD_MANAGE/New/1594282443102/948a9b16-da98-4a99-9ad0-55300007a2f7.jpg;http://comac-mom-prod.oss-cn-shanghai-internal.aliyuncs.com/MAIN_FRR_RECORD_MANAGE/New/1594282452812/CR-NEIAS-105(20)_页面_2.jpg;http://comac-mom-prod.oss-cn-shanghai-internal.aliyuncs.com/MAIN_FRR_RECORD_MANAGE/New/1594282456678/CR-NEIAS-105(20)_页面_3.jpg;http://comac-mom-prod.oss-cn-shanghai-internal.aliyuncs.com/MAIN_FRR_RECORD_MANAGE/New/1594282460417/CR-NEIAS-105(20)_页面_4.jpg, disAttachExist=null)";
+		inputStr = "(frrCode=R-15DC0003090, frrFileName=1dd4afb8-1e5b-43d9-ab4e-0d32d08da08a.pdf, frrFileUrl=http://comac-mom-prod.oss-cn-shanghai-internal.aliyuncs.com/FRR_PDF/PDF_FILES/1dd4afb8-1e5b-43d9-ab4e-0d32d08da08a.pdf, drawCode=, drawFileName=null, drawFileUrl=null, sortie=, dataFrom=null, sendTime=2020-07-09 16:21:20, state=, frrId=356, frrType=FRR非转移, frrFrom=null, frrLoginuser=201514, aircraftType=C919, disposition=DispositionDTO(useAsIt=1, rework=, repair=, scrap=, secondTestPassed=, returnToSupplier=, initialTreatment=), respDepartmentName=SUPP, keyItem=一般件, discrepancy=严重不合格, enginePicExist=null, enginePinguExist=null, engineAttachExist=null, engineFileExist=null, correctFileExist=null, scrapFileExist=null, frrPicExist=http://comac-mom-prod.oss-cn-shanghai-internal.aliyuncs.com/MAIN_FRR_RECORD_MANAGE/New/1594283641242/f48ef1fc-386e-4e46-9b14-56c4a4894906.jpg;http://comac-mom-prod.oss-cn-shanghai-internal.aliyuncs.com/MAIN_FRR_RECORD_MANAGE/New/1594283650633/CR-NEIAS-106(20)_A_页面_2.jpg;http://comac-mom-prod.oss-cn-shanghai-internal.aliyuncs.com/MAIN_FRR_RECORD_MANAGE/New/1594283653093/CR-NEIAS-106(20)_A_页面_3.jpg;http://comac-mom-prod.oss-cn-shanghai-internal.aliyuncs.com/MAIN_FRR_RECORD_MANAGE/New/1594283659759/CR-NEIAS-106(20)_A_页面_4.jpg;http://comac-mom-prod.oss-cn-shanghai-internal.aliyuncs.com/MAIN_FRR_RECORD_MANAGE/New/1594283661975/CR-NEIAS-106(20)_A_页面_5.jpg, disAttachExist=null)\r\n";
+		inputStr = "(frrCode=R-15DC0003091, frrFileName=71625adc-85cc-471e-bf14-179157aaa6ea.pdf, frrFileUrl=http://comac-mom-prod.oss-cn-shanghai-internal.aliyuncs.com/FRR_PDF/PDF_FILES/71625adc-85cc-471e-bf14-179157aaa6ea.pdf, drawCode=, drawFileName=null, drawFileUrl=null, sortie=, dataFrom=null, sendTime=2020-07-13 13:37:48, state=, frrId=555, frrType=FRR非转移, frrFrom=null, frrLoginuser=203053, aircraftType=C919, disposition=DispositionDTO(useAsIt=2, rework=, repair=, scrap=, secondTestPassed=, returnToSupplier=, initialTreatment=), respDepartmentName=SUPP, keyItem=一般件, discrepancy=严重不合格, enginePicExist=null, enginePinguExist=null, engineAttachExist=null, engineFileExist=null, correctFileExist=null, scrapFileExist=null, frrPicExist=http://comac-mom-prod.oss-cn-shanghai-internal.aliyuncs.com/MAIN_FRR_RECORD_MANAGE/New/1594618657894/CR-SAEC-025（20）_页面_1.jpg;http://comac-mom-prod.oss-cn-shanghai-internal.aliyuncs.com/MAIN_FRR_RECORD_MANAGE/New/1594618661271/CR-SAEC-025（20）_页面_2.jpg, disAttachExist=http://comac-mom-prod.oss-cn-shanghai-internal.aliyuncs.com/MAIN_FRR_RECORD_MANAGE/New/1594618653713/TRU(1902006、1902007) _Supplier MINOR Discrepancy List.pdf)";
+		inputStr = "(frrCode=R-15DC0003092, frrFileName=4078cabd-e01a-4138-80e8-5e45a02a23b4.pdf, frrFileUrl=http://comac-mom-prod.oss-cn-shanghai-internal.aliyuncs.com/FRR_PDF/PDF_FILES/4078cabd-e01a-4138-80e8-5e45a02a23b4.pdf, drawCode=, drawFileName=null, drawFileUrl=null, sortie=, dataFrom=null, sendTime=2020-07-13 14:02:52, state=, frrId=558, frrType=FRR非转移, frrFrom=null, frrLoginuser=203053, aircraftType=C919, disposition=DispositionDTO(useAsIt=1, rework=, repair=, scrap=, secondTestPassed=, returnToSupplier=, initialTreatment=), respDepartmentName=SUPP, keyItem=一般件, discrepancy=严重不合格, enginePicExist=null, enginePinguExist=null, engineAttachExist=null, engineFileExist=null, correctFileExist=null, scrapFileExist=null, frrPicExist=http://comac-mom-prod.oss-cn-shanghai-internal.aliyuncs.com/MAIN_FRR_RECORD_MANAGE/New/1594620218457/CR-SAEC-026（20)_页面_1.jpg;http://comac-mom-prod.oss-cn-shanghai-internal.aliyuncs.com/MAIN_FRR_RECORD_MANAGE/New/1594620221210/CR-SAEC-026（20)_页面_2.jpg, disAttachExist=null)";
+		inputStr = "(frrCode=R-15DC0003094, frrFileName=94642792-2d59-428e-93a0-3484d14992ca.pdf, frrFileUrl=http://comac-mom-prod.oss-cn-shanghai-internal.aliyuncs.com/FRR_PDF/PDF_FILES/94642792-2d59-428e-93a0-3484d14992ca.pdf, drawCode=, drawFileName=null, drawFileUrl=null, sortie=, dataFrom=null, sendTime=2020-07-14 14:20:03, state=, frrId=629, frrType=FRR非转移, frrFrom=null, frrLoginuser=204201, aircraftType=C919, disposition=DispositionDTO(useAsIt=4, rework=, repair=, scrap=, secondTestPassed=, returnToSupplier=, initialTreatment=), respDepartmentName=SUPP, keyItem=一般件, discrepancy=严重不合格, enginePicExist=null, enginePinguExist=null, engineAttachExist=null, engineFileExist=null, correctFileExist=null, scrapFileExist=null, frrPicExist=http://comac-mom-prod.oss-cn-shanghai-internal.aliyuncs.com/MAIN_FRR_RECORD_MANAGE/New/1594707550974/aa089c77-f648-465b-aa59-21c7d94903e9.jpg;http://comac-mom-prod.oss-cn-shanghai-internal.aliyuncs.com/MAIN_FRR_RECORD_MANAGE/New/1594707556187/CR-PFC-003-(20)_D_页面_2.jpg, disAttachExist=null)";
+		// inputStr = inputStr.substring(1);
+		inputStr = inputStr.substring(1, inputStr.length() - 1);
+		inputStr = inputStr.replace("(", "{").replace(")", "}").replace("=", ": ");
+		StringBuffer sb = new StringBuffer();
+		// inputStr
+
+		String[] ss = inputStr.split(", ");
+		for (String s : ss) {
+			String[] is = s.split(": ");
+
+			for (int m = 0; m < is.length; m++) {
+				if (m == 0) {
+					sb.append("\"").append(is[m]).append("\"").append(": ");
+				} else {
+					if (is[m].contains("{") || is[m].contains("}")) {
+						sb.append(" \r\n");
+						sb.append(is[m]);
+						sb.append(" \r\n");
+					} else {
+						if (is[m].trim().equals("")) {
+							is[m] = "\"\"";
+						}
+						sb.append("\"");
+						sb.append(is[m]);
+						sb.append("\"");
+					}
+					sb.append(", ");
+				}
+				if (is.length == 1) {
+					sb.append("\"\",");
+				}
+
+			}
+			sb.append(" \r\n");
+
+		}
+		String output = sb.toString();
+		output = output.replace("\"null\"", "null");
+		System.out.println(output.replace("DispositionDTO", " "));
+
+	}
+}
---
+++ base/base-FORM/src/main/java/Masdasd.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Masdasd {
+
+	public static void main(String...strings) {
+
+	}
+}

50cdbe8b6189526bfbbedaa7774a72855d571efb
shiyi <shiyi25928988@163.com>
2020-10-02 22:17:31 +0800

    1231

--- base/base-business-custom-001/src/main/java/custom/business/Main.java
+++ base/base-business-custom-001/src/main/java/custom/business/Main.java
@@ -4,10 +4,8 @@ import java.io.IOException;
 
 import core.annotation.PropertiesFile;
 import core.booter.ServiceBooter;
-import lombok.extern.slf4j.Slf4j;
 
 @PropertiesFile(files = { "application.properties" })
-@Slf4j
 public class Main {
 	public static void main(String... strings) {
 		try {
